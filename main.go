c8e6dcd9640d7c44ce5577b8a4d6e44c3f7383c3
chenmin <453733011@qq.com>
2017-03-13 16:52:28 +0800

    提交

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	beego.Run()
 }
 
-// This is the controller that this application uses111
+// 111This is the controller that this application uses111
 type mainController struct {
 	beego.Controller
 }
